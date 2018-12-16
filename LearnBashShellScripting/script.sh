18763b5a30337f404d52c7dde88bae998ad373a0
cc <cc@remus.ro>
2018-12-16 23:18:42 +0200

    Initial Commit bash lesson

---
+++ LearnBashShellScripting/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Scripting rules!"
+echo "#!/bin/bash"
+echo "chmod 755 your_script"
+echo "./your_script.sh"
+echo "ps -fp (pid ./script& )"
+echo "pstree -p pid"
---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains the files user for bash scripting used for prepairing for a a new job and going again through documentation.
